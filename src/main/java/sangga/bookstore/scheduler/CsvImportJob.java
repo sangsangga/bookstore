52ffeac5792143d1343a6b80cc52bd9dd01dae54
sangsangga <wandaputrasangga@gmail.com>
2024-07-17 05:02:40 +0700

    change logging

--- src/main/java/sangga/bookstore/scheduler/CsvImportJob.java
+++ src/main/java/sangga/bookstore/scheduler/CsvImportJob.java
@@ -1,30 +1,29 @@
 package sangga.bookstore.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CsvImportJob implements Job {
-    private static final Logger logger = LoggerFactory.getLogger(CsvImportJob.class);
     private final JobLauncher jobLauncher;
     private final org.springframework.batch.core.Job csvImporterJob;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
-            logger.info("Starting job");
+            log.info("Starting job");
             jobLauncher.run(csvImporterJob, new JobParameters());
-            logger.info("Job executed");
+            log.info("Job executed");
         } catch (Exception e) {
-            logger.error("Error executing job", e);
+            log.error("Error executing job", e);
         }
     }
 }
