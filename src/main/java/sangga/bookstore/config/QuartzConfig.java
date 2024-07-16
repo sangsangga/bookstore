58710e0733232e8ab98e163a2eb4f88f83b5c328
sangsangga <wandaputrasangga@gmail.com>
2024-07-17 04:42:11 +0700

    remove wildcard import

--- src/main/java/sangga/bookstore/config/QuartzConfig.java
+++ src/main/java/sangga/bookstore/config/QuartzConfig.java
@@ -1,7 +1,10 @@
 package sangga.bookstore.config;
 
-import org.quartz.*;
-import org.springframework.context.annotation.Bean;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sangga.bookstore.scheduler.CsvImportJob;
 
